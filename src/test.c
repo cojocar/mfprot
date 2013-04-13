7ea839f7323d659eec3046891f49f59aee1be73a
Lucian Cojocar <cojocar@gmail.com>
2013-04-13 16:52:00 +0000

    libmfprot: test code

--- src/test.c
+++ src/test.c
@@ -7,9 +7,13 @@ main(int argc, char *argv[])
 {
 	mfprot_device d = mfprot_get_device("/dev/ttyAMA0");
 	uint8_t status;
+	uint8_t id[7];
 
 	status = mfprot_get_status(d);
 	mfprot_display_status(stdout, status);
+
+	mfprot_get_uid(d, id);
+	mfprot_display_uid(stdout, id);
 
 	mfprot_release_device(d);
 }
